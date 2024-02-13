1762fae6d7d2821a298804b13b725dba84ddac68
Amarin Sam <amarinsam@gmail.com>
2024-02-13 15:55:44 -0800

    Complete deleting a todo

--- datastore/index.js
+++ datastore/index.js
@@ -47,34 +47,73 @@ exports.readAll = (callback) => {
 //   return {id: fileName, text: fileName}
 // })
 
+// exports.readOne = (id, callback) => {
+//   var text = items[id];
+//   if (!text) {
+//     callback(new Error(`No item with id: ${id}`));
+//   } else {
+//     callback(null, { id, text });
+//   }
+// };
+
 exports.readOne = (id, callback) => {
-  var text = items[id];
-  if (!text) {
-    callback(new Error(`No item with id: ${id}`));
-  } else {
-    callback(null, { id, text });
-  }
+  fs.readFile(path.join(exports.dataDir, `${id}.txt`), (err, data) => {
+    if (err) {
+      callback(err);
+    } else {
+      callback(null, {'id': id, 'text': String(data)});
+    }
+  });
 };
 
+// Path: exports.dataDir/`${id}.txt`
+
 exports.update = (id, text, callback) => {
-  var item = items[id];
-  if (!item) {
-    callback(new Error(`No item with id: ${id}`));
-  } else {
-    items[id] = text;
-    callback(null, { id, text });
-  }
+  fs.readFile(path.join(exports.dataDir, `${id}.txt`), (err, data) => {
+    if (err) {
+      callback(err);
+    } else {
+      fs.writeFile(path.join(exports.dataDir, `${id}.txt`), text, (err) => {
+        if (err) {
+          callback(err);
+        } else {
+          callback(null, {'id': id, 'text': text});
+        }
+      });
+    }
+  });
+
+
+  // var item = items[id];
+  // if (!item) {
+  //   callback(new Error(`No item with id: ${id}`));
+  // } else {
+  //   items[id] = text;
+  //   callback(null, { id, text });
+  // }
 };
 
 exports.delete = (id, callback) => {
-  var item = items[id];
-  delete items[id];
-  if (!item) {
-    // report an error if item not found
-    callback(new Error(`No item with id: ${id}`));
-  } else {
-    callback();
-  }
+  fs.rm(path.join(exports.dataDir, `${id}.txt`), (err) => {
+    callback(err);
+  });
+
+  // exports.delete = (id, callback) => {
+  //   var filepath = path.join(exports.dataDir, `${id}.txt`);
+  //   fs.unlink(filepath, (err) => {
+  //     callback(err);
+  //   });
+  //   };
+
+
+  // var item = items[id];
+  // delete items[id];
+  // if (!item) {
+  //   // report an error if item not found
+  //   callback(new Error(`No item with id: ${id}`));
+  // } else {
+  //   callback();
+  // }
 };
 
 // path.join(dataDir, 'dyanmicIDfilename')
